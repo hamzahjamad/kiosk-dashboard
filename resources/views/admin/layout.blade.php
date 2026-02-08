5a308a51a98deefc399b58a25e5f3afb18bd6580
Hamzah Jamad <hamzahjamad@gmail.com>
2026-02-09 07:12:52 +0800

    feat: added icon

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -5,7 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title') - Kiosk Dashboard Admin</title>
-    
+
+    <link rel="icon" type="image/svg+xml" href="{{ asset('favicon.svg') }}">
+
     <!-- FontAwesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
     
